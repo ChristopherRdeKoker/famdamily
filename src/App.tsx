d766643c478b6582a428ac3703af905f5e5ae11a
ChristopherRdeKoker <christopherdekoker@gmail.com>
2023-10-29 09:00:46 +0200

    some initial changes an d cleanup from raw vite app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import "./App.css";
-
-function App() {
+export default function App() {
   return (
-    <>
-      <div>
-        <h2>Init of famdamily with vite + tailwind</h2>
-      </div>
-    </>
+    <div className=" w-100% h-[100vh] flex-grow bg-gradient-to-r from-indigo-300 via-purple-500 to-pink-500">
+      <h2 className="mx-auto flex flex-col p-4">Init of famdamily with vite + tailwind</h2>
+    </div>
   );
 }
-
-export default App;
